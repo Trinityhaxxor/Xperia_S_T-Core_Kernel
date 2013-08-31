c6de923a54a7e7b5455866e1263a18811bf957a7
Leon Ma <xindong.ma@intel.com>
2013-08-31 02:44:15 -0500

    LMK: Optimize lowmem_shrink.
    By comparing with selected_oom_score_adj instead of min_score_adj,
    we may do less calculation.
    
    Signed-off-by: Leon Ma <xindong.ma@intel.com>
    
    Signed-off-by: Paul Reioux <reioux@gmail.com>

--- drivers/staging/android/lowmemorykiller.c
+++ drivers/staging/android/lowmemorykiller.c
@@ -213,7 +213,7 @@ count:%d index %d\n",
 			return LMK_BUSY;
 		}
 		oom_score_adj = p->signal->oom_score_adj;
-		if (oom_score_adj < min_score_adj) {
+		if (oom_score_adj < selected_oom_score_adj) {
 			task_unlock(p);
 			continue;
 		}
@@ -236,13 +236,9 @@ run time %ld, oom_score_adj %d\n", p->pid,
 		task_unlock(p);
 		if (tasksize <= 0)
 			continue;
-		if (selected) {
-			if (oom_score_adj < selected_oom_score_adj)
-				continue;
-			if (oom_score_adj == selected_oom_score_adj &&
-			    tasksize <= selected_tasksize)
-				continue;
-		}
+		if (selected && oom_score_adj == selected_oom_score_adj &&
+			tasksize <= selected_tasksize)
+			continue;
 		selected = p;
 		selected_tasksize = tasksize;
 		selected_oom_score_adj = oom_score_adj;
