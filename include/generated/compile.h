685138ce1534e2a068c586a539d7c2ddede448b5
trinityhaxxor@gmail.com <trinityhaxxor@gmail.com>
2013-09-15 22:44:27 +0300

    Initial Files

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 6 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#6 SMP PREEMPT Fri Sep 13 21:41:07 AST 2013"
+#define LINUX_COMPILE_BY "TrinityHaxxorX"
+#define LINUX_COMPILE_HOST "TrinityServer"
+#define LINUX_COMPILER "gcc version 4.7.2 (crosstool-NG hg+default-67df47c6ccd0 - Cortex-A8) "
